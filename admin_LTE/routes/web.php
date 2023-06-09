0bd6ab4219e1b300515c088f7568d69187f7e3ad
JALA MEDIA COMP <maryatulkippiyah01@gmail.com>
2023-06-09 20:18:44 +0700

    commit kelima:Laravel CRUD dengan Query Builder

--- admin_LTE/routes/web.php
+++ admin_LTE/routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CastController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -23,4 +25,14 @@ Route::get('/table', function () {
 Route::get('/data-tables', function () {
     return view('data-tables');
 });
+
+Route::get('/cast', [CastController::class, 'index'])->name('cast-index');
+Route::get('/cast/create', [CastController::class, 'create'])->name('cast-create');
+Route::post('/cast', [CastController::class, 'store'])->name('cast-store');
+Route::get('/cast/{cast_id}', [CastController::class, 'show'])->name('cast-show');
+Route::get('/cast/{cast_id}/edit', [CastController::class, 'edit'])->name('cast-edit');
+Route::post('/cast/{cast_id}/update', [CastController::class, 'update'])->name('cast-update');
+Route::get('/cast/{cast_id}/delete', [CastController::class, 'destroy'])->name('cast-destroy');
+
+
 
